66fc594d5c8b147e564a00aacb5f4fd1918c9f7c
WallaceRepo <sophiawallacedev@gmail.com>
2020-12-01 10:41:40 -0600

    Deleted public and src folders from the root folder. Those folders are inside client folder

--- src/redux/catalog/catalog.selector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectCatalog = state => state.catalog;
-
-export const selectCatalogSections = createSelector(
-    [selectCatalog],
-    catalog => catalog.sections
-);
